b69c27f35d78051d3f1fb6fe8a7daca037b4995b
Stiven Gomez <stivengomez@soyyo.co>
2023-09-13 13:21:07 -0500

    Add components header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { Header } from './Components/Header';
 import './App.css'
+import { version } from '../package.json';
 
 function App() {
-
+  console.log(version);
   return (
     <>
-      
+      <Header />
       <div className="card">
-        
+        {version}
       </div>
       
     </>
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return(
+        <nav>
+
+        <span>Stiven Gomez</span>
+
+        </nav>
+    );
+}
