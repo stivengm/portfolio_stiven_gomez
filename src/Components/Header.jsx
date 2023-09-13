2f36fd7e67e1f3642c573c09cc3cbfaa861619ed
Stiven Gomez <stivengomez@soyyo.co>
2023-09-13 14:30:23 -0500

    Fix header.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,16 @@ import './Header.css';
 export function Header() {
     return(
         <nav className='navbar'>
-            <img src={codeLogo} className='codeLogo' alt="codeLogo" />
-            <span>Stiven Gomez</span>
+            <div className='name-and-code'>
+                <img src={codeLogo} className='codeLogo' alt="codeLogo" />
+                <span className='name'>Stiven Gomez</span>
+            </div>
+            <div className='options'>
+                <span>Inicio</span>
+                <span>Sobre mi</span>
+                <span>Proyectos</span>
+                <span>Contacto</span>
+            </div>
         </nav>
     );
 }
