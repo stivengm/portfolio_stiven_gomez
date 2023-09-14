0a7fb2bdd9aea08547056c136f1b42c0a3be1ead
Stiven Gomez <stivengomez@soyyo.co>
2023-09-13 19:19:33 -0500

    Fix biografy.

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,10 @@ function App() {
   return (
     <>
       <Header />
-      <p>Desarrollador de Software con más de 5 años de experiencia, he trabajado en múltiples proyectos de desarrollo de software, incluyendo aplicaciones web, móviles. Poseo habilidades en lenguajes de programación como Flutter, Angular, NodeJs, ReactJs y WebPack incluyendo despliegues en tiendas de aplicaciones tales como GooglePlay y AppStore. Estoy familiarizado con metodologías ágiles y prácticas de desarrollo modernas. Además, estoy comprometido con la mejora continua y la actualización constante de mis habilidades técnicas para ofrecer soluciones de alta calidad y con las últimas tecnologías disponibles.</p>
+      <div className='auto-biografia'>
+        <p>Desarrollador de Software con más de 5 años de experiencia, he trabajado en múltiples proyectos de desarrollo de software, incluyendo aplicaciones web, móviles. Poseo habilidades en lenguajes de programación como Flutter, Angular, NodeJs, ReactJs y WebPack incluyendo despliegues en tiendas de aplicaciones tales como GooglePlay y AppStore. Estoy familiarizado con metodologías ágiles y prácticas de desarrollo modernas. Además, estoy comprometido con la mejora continua y la actualización constante de mis habilidades técnicas para ofrecer soluciones de alta calidad y con las últimas tecnologías disponibles.</p>
+      </div>
       <div className="card">
-        {version}
         <img src={flutter} alt="Flutter" />
         <Projects />
         <Works />
