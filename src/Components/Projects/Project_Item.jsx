6305a49f4595c2918b7b75d08a7fab9a54c6d08a
Stiven Gomez <stivengomez@soyyo.co>
2023-09-14 16:44:15 -0500

    Fix projects.

--- src/Components/Projects/Project_Item.jsx
+++ src/Components/Projects/Project_Item.jsx
@@ -1,16 +1,31 @@
 import './Projects.css';
 
 export function ProjectItem({ name, img, description, personalProject, stors = [] }) {
-    console.log(name);
     return(
         <div className='project-item'>
             <div className='img-project'>
                 <img className='project-img' src={img} alt={name} />
             </div>
             <div className='information-project'>
-                <strong className='name-project'>{name}</strong>
-                <p>{description}</p>
-                <span className='personal-project'>{ personalProject ? 'Proyecto personal' : '' }</span>
+                <div className='info-project'>
+                    <strong className='name-project'>{name}</strong>
+                    <div>
+                        <span className='personal-project'>{ personalProject ? 'Proyecto personal' : '' }</span>
+                    </div>
+                    <p>{description}</p>
+                </div>
+                {
+                    stors ? 
+                    <div className='stores-projects'>
+                        {
+                            stors[0].publish ? <img className='img-store' src={stors[0].img_store} alt={name} /> : <secion></secion>
+                        }
+                        {
+                            stors[1].publish ? <img className='img-store' src={stors[1].img_store} alt={name} /> : <secion></secion>
+                        }
+                    </div> :
+                    <section></section>
+                }
             </div>
         </div>
     );
