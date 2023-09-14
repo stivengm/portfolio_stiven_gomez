65062899b8f7df7a95f628077aac945ddcca237e
Stiven Gomez <stivengomez@soyyo.co>
2023-09-14 13:15:29 -0500

    Add component Loader.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import { Header } from './Components/Layout/Header';
 import { Footer } from './Components/Layout/Footer';
+import { Loader } from './Components/Loader/Loader';
 import { ContactMe } from './Components/Contact/Contact';
 import { Skills } from './Components/Skills/Skills';
 import { Projects } from './Components/Projects/Projects';
@@ -14,11 +15,15 @@ import './App.css';
 function App() {
 
   const [data, setData] = useState({});
+  const [isLoading, setLoader] = useState(true);
 
   useEffect(() => {
     axios.get('https://portfolio-51fbb-default-rtdb.firebaseio.com/profile.json')
       .then((response) => {
         setData(response.data);
+        setTimeout(() => {
+          setLoader(false);
+        }, 1000);
       })
       .catch((error) => {
         throw error;
@@ -27,22 +32,25 @@ function App() {
 
   return (
     <>
-      <Header name={data.name} />
-      <div id='landing-page' className='landing-page' style={{ height: 'calc(' + window.innerHeight +'px - 60px)' }}>
-        <div className='auto-biografia'>
-          <h1>{data.profession}</h1>
-          <p>{data.description}</p>
-        </div>
-        <Skills skills={data.skills} />
-        <img className='icon-arrow-down' src={arrowDown} alt="arrowDown"/>
-      </div>
-      <div className="card">
-        <ContactMe contact={data.contact} />
-        {/* <Projects /> */}
-        <Works works={data.works} />
-      </div>
-      <Footer />
-      
+      {
+        isLoading ? <Loader /> :
+          <>
+            <Header name={data.name} /><div id='landing-page' className='landing-page' style={{ height: 'calc(' + window.innerHeight + 'px - 60px)' }}>
+              <div className='auto-biografia'>
+                <h1>{data.profession}</h1>
+                <p>{data.description}</p>
+              </div>
+              <Skills skills={data.skills} />
+              <img className='icon-arrow-down' src={arrowDown} alt="arrowDown" />
+              </div><div className="card">
+                <ContactMe contact={data.contact} />
+                {/* <Projects /> */}
+                <Works works={data.works} />
+              </div>
+              <Footer />
+          </>
+
+      }
     </>
   )
 }
