70c648b9daa86bd911884570afd6e36f77690634
Stiven Gomez <stivengomez@soyyo.co>
2023-09-13 19:47:20 -0500

    Fix work items.

--- src/Components/Work-Item.jsx
+++ src/Components/Work-Item.jsx
@@ -4,10 +4,11 @@ import flutter from '../assets/flutter_icon.svg';
 export function WorkItem() {
     return (
         <div className="item-work">
-            <img src={flutter} alt="" />
+            <img src={flutter} alt="flutter_logo" />
             <div className='info-work'>
                 <strong className='work-name'>Senior Flutter Developer</strong>
                 <p className='entity-work'>SoyYo - Identidad digital</p>
+                <p className='time-work'>Jun. 2022 - ACTUALMENTE</p>
                 <p className='description-work'>Desarrollo de aplicaciones en Flutter - Dart, uso de Patron BLoC, creación e integración de SDK en WebPack.</p>
             </div>
         </div>
