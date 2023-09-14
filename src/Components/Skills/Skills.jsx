c06ee38702cd66260b27d6e33164f5d6c325d076
Stiven Gomez <stivengomez@soyyo.co>
2023-09-14 12:22:37 -0500

    Fix Skills from service.

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,19 +1,13 @@
 import './Skills.css';
 import { SkillItem } from './Skill-Item';
 
-import flutter from '../../assets/flutter_icon.svg';
-import angular from '../../assets/angular_icon.svg';
-import nodeJs from '../../assets/nodejs_icon.svg';
-import react from '../../assets/react_icon.svg';
-
-export function Skills() {
+export function Skills({ skills = [] }) {
+    var newSkills = skills;
     return(
         <div className='skills'>
-            <SkillItem img={flutter} name='flutter_logo' />
-            <SkillItem img={angular} name='flutter_logo' />
-            <SkillItem img={nodeJs} name='flutter_logo' />
-            <SkillItem img={react} name='flutter_logo' />
-            {/* <img src={flutter} alt="flutter_logo" /> */}
+        {
+            newSkills.map((skill) => <SkillItem key={skill.id} img={skill.img_icon} name={skill.name} />)
+        }
         </div>
     );
 }
