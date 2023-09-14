c9c753685b24aa89fecffdd1440c52c9d81a5b4c
Stiven Gomez <stivengomez@soyyo.co>
2023-09-13 21:37:59 -0500

    Fix design home.

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,18 @@ import { Skills } from './Components/Skills/Skills';
 import { Projects } from './Components/Projects/Projects';
 import { Works } from './Components/Work/Works';
 import './App.css'
-import { version } from '../package.json';
-
-import flutter from './assets/flutter_icon.svg';
 
 function App() {
-  console.log(version);
   return (
     <>
       <Header />
-      <div className='auto-biografia'>
-        <p>Desarrollador de Software con más de 5 años de experiencia, he trabajado en múltiples proyectos de desarrollo de software, incluyendo aplicaciones web, móviles. Estoy familiarizado con metodologías ágiles y prácticas de desarrollo modernas. Además, estoy comprometido con la mejora continua y la actualización constante de mis habilidades técnicas para ofrecer soluciones de alta calidad y con las últimas tecnologías disponibles.</p>
-      </div>
-
-      <div className="card">
+      <div id='landing-page' className='landing-page' style={{ height: 'calc(' + window.innerHeight +'px - 60px)' }}>
+        <div className='auto-biografia'>
+          <p>Desarrollador de Software con más de 5 años de experiencia, he trabajado en múltiples proyectos de desarrollo de software, incluyendo aplicaciones web, móviles. Estoy familiarizado con metodologías ágiles y prácticas de desarrollo modernas. Además, estoy comprometido con la mejora continua y la actualización constante de mis habilidades técnicas para ofrecer soluciones de alta calidad y con las últimas tecnologías disponibles.</p>
+        </div>
         <Skills />
+      </div>
+      <div className="card">
         {/* <img src={flutter} alt="Flutter" /> */}
         {/* <Projects /> */}
         <Works />
