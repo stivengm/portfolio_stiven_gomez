756ddcc110aa11137ea2fce6a20b8a4cfd060177
Stiven Gomez <stivengomez@soyyo.co>
2023-09-14 11:39:38 -0500

    Add service information.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
 import { Header } from './Components/Layout/Header';
 import { Footer } from './Components/Layout/Footer';
 import { Skills } from './Components/Skills/Skills';
 import { Projects } from './Components/Projects/Projects';
 import { Works } from './Components/Work/Works';
 import arrowDown from './assets/arrow_down_icon.svg';
-import './App.css'
+
+import './App.css';
 
 function App() {
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get('https://portfolio-51fbb-default-rtdb.firebaseio.com/profile.json')
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }, [setData]);
+
   return (
     <>
       <Header />
       <div id='landing-page' className='landing-page' style={{ height: 'calc(' + window.innerHeight +'px - 60px)' }}>
         <div className='auto-biografia'>
-          <p>Desarrollador de Software con más de 5 años de experiencia, he trabajado en múltiples proyectos de desarrollo de software, incluyendo aplicaciones web, móviles. Estoy familiarizado con metodologías ágiles y prácticas de desarrollo modernas. Además, estoy comprometido con la mejora continua y la actualización constante de mis habilidades técnicas para ofrecer soluciones de alta calidad y con las últimas tecnologías disponibles.</p>
+          <p>{data.description}</p>
+          {/* <p>Desarrollador de Software con más de 5 años de experiencia, he trabajado en múltiples proyectos de desarrollo de software, incluyendo aplicaciones web, móviles. Estoy familiarizado con metodologías ágiles y prácticas de desarrollo modernas. Además, estoy comprometido con la mejora continua y la actualización constante de mis habilidades técnicas para ofrecer soluciones de alta calidad y con las últimas tecnologías disponibles.</p> */}
         </div>
         <Skills />
         <img className='icon-arrow-down' src={arrowDown} alt="arrowDown"/>
