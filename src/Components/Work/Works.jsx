6f53807cf6ec2b8738c4707d9e749a645c7dec1a
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-04-03 14:39:14 -0500

    FIX - Ajuste de organización de trabajos.

--- src/Components/Work/Works.jsx
+++ src/Components/Work/Works.jsx
@@ -3,13 +3,14 @@ import './Works.css';
 
 export function Works({ works = [] }) {
     var newWorks = works;
+    var newWorksReverse = newWorks.sort((a, b) => b.id - a.id);
 
     return(
         <div className="works">
             <h1>Trabajos</h1>
             <div className='works-section'>
                 {
-                    newWorks.map((work) => <WorkItem key={work.id} workName={work.position} entityWork={work.name} img={work.img} timeWork={work.time} descriptionWork={work.description} />)
+                    newWorksReverse.map((work) => <WorkItem key={work.id} workName={work.position} entityWork={work.name} img={work.img} timeWork={work.time} descriptionWork={work.description} />)
                 }
             </div>
         </div>
