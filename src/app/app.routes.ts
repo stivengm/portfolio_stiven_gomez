2146957791f4d1caccd037efe876f21c34ec6b0a
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-06-07 22:15:18 -0500

    FIX - Ajuste de header con presentacion.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Home,
+        pathMatch: 'full'
+    },
+    {
+        path: "**",
+        redirectTo: '/',
+    }
+];
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
+import { Presentation } from '../../shared/presentation/presentation';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    Header,
+    Presentation,
+    Footer
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
